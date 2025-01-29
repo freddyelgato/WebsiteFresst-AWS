919a30b3f6d9f0b526e7b06f27d46d3442fa771b
freddyelgato <freddymoreirala@gmail.com>
2025-01-29 04:29:37 -0500

    feat: :sparkles: Create the new Microservice about Edit Orders
    Files and Folders organized with a structure

---
+++ BackEnd/Domains/Orders/EditOrder/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"edit-order/config/cors"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(cors.GetCORSConfig())
+	//[ginshot-routes]
+	//[HttpGET] Ping to edit-order API
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return router
+}
+	
