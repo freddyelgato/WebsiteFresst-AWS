68aab37a84121042b91c57e83a5b613918f8eedc
freddyelgato <freddymoreirala@gmail.com>
2025-02-06 06:53:27 -0500

    feat: :ambulance: Change in the search.js

--- BackEnd/Domains/Product/Product_Search(ApiRest)/routes/search.js
+++ BackEnd/Domains/Product/Product_Search(ApiRest)/routes/search.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, { 
+    dbName: "spaceofsport_products", // 🔥 Forzar la base de datos correcta
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB')).catch(err => console.error('MongoDB connection error:', err));
