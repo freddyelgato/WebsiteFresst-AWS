55c00f3c1dacf86fafe06b9eb4720217f72633b9
freddyelgato <freddymoreirala@gmail.com>
2025-02-13 09:09:38 -0500

    test

--- BackEnd/databases/Products/init-mongo.sh
+++ BackEnd/databases/Products/init-mongo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongoimport --host localhost --db spaceofsport_products --collection products --type json --file /data/products.json --jsonArray
+mongoimport --host 54.89.143.59 --db spaceofsport_products --collection products --type json --file /data/products.json --jsonArray
